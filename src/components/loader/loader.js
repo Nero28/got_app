5e1c29dabc4e98573ce4f694d217408f4d529509
Nero28 <shackiy.s@yandex.ru>
2020-06-16 11:49:20 +0300

    first commit

---
+++ src/components/loader/loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loader.module.css';
+
+const Loader = () => {
+    return (
+        <div className="loadingio-spinner-spinner-led01qmzza"><div className="ldio-jhfve4cp6w">
+            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    )
+};
+
+export default Loader;
